e518fff4a906ba81aef8c0f9a42cfac9a47f0919
root <root@b7635c4f7eb2>
2020-04-08 07:35:56 +0000

    initialized v 13.0

--- database_rollback/static/src/js/db_rollback.js
+++ database_rollback/static/src/js/db_rollback.js
@@ -1,7 +1,6 @@
 odoo.define('web.database_rollback',function (require) {
 
     "use strict";
-
     var Widget = require('web.Widget');
     var SystrayMenu = require('web.SystrayMenu');
     var web_client = require('web.web_client');
@@ -16,16 +15,14 @@ odoo.define('web.database_rollback',function (require) {
             this.$el.find('.activate').on('click', function(ev) {
                     self.$el.find('.activate').css("background-color", "green").css("color", "white");
                     var func = '/database_rollback/activate';
-                    self._rpc({route:func, params:{}}).done(function(res) {
-                    });
+                    self._rpc({route:func, params:{}});
             });
 
             this.$el.find('.rollback').on('click', function(ev) {
                     self.$el.find('.activate').css("background-color", "buttonface")
                         .css("color", "#777");
                     var func = '/database_rollback/rollback';
-                    self._rpc({route:func, params:{}}).done(function(res) {
-                    });
+                    self._rpc({route:func, params:{}});
             });
         },
     });
